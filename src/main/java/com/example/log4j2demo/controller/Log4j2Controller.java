0ca25fda1b3ff6c0579027db63cc5f2699de736d
Ricardo Ropelli Peña <ricardo-penha@hotmail.com>
2019-04-10 10:04:22 -0300

    Adicionando parametros na ThreadContext para que seja usada no log, por exemplo algum clientID que vem via header ou query param.

--- src/main/java/com/example/log4j2demo/controller/Log4j2Controller.java
+++ src/main/java/com/example/log4j2demo/controller/Log4j2Controller.java
@@ -1,8 +1,10 @@
 package com.example.log4j2demo.controller;
 
+import org.apache.logging.log4j.ThreadContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,7 +13,10 @@ public class Log4j2Controller {
     private static final Logger LOGGER = LoggerFactory.getLogger(Log4j2Controller.class);
 
     @GetMapping("/")
-    public void log() {
+    public void log(@RequestParam(name = "bu") String bu) {
+        //TODO: Esse ponto pode ser incluido no filter para que seja trafegado na thread e utilizado pelos logs.
+        ThreadContext.put("bu", bu);
+
         LOGGER.debug("Debugging log");
         LOGGER.info("Info log");
         LOGGER.warn("Hey, This is a warning!");
